690e88b1d832a3f4dd7541c7ab55149526defcb5
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-03-05 20:15:16 +0100

    style search btn in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,38 +69,9 @@
                 <!-- Right Side Of Navbar -->
                 <ul class="nav_right">
                     <li>
-                        <a id="search" href="#"> Search <i class="fa fa-search" aria-hidden="true"></i></a>
-                    </li>
-                    
-                    <!-- Authentication Links -->
-                    {{-- @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest --}}
+                        <a href="#"> Search </a>
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </li> 
                 </ul>
             </div>
         </div>
